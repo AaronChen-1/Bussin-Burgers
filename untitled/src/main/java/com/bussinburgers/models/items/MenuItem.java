29cc3f1783c76a5f89ba2207f00b089b6517bc91
arone415 <105476658+arone415@users.noreply.github.com>
2025-11-13 16:29:46 -0800

    fixed my menu item

--- untitled/src/main/java/com/bussinburgers/models/items/MenuItem.java
+++ untitled/src/main/java/com/bussinburgers/models/items/MenuItem.java
@@ -2,15 +2,22 @@ package com.bussinburgers.models.items;
 
 public abstract class MenuItem {
     protected String name;
+    protected double basePrice;
 
-    public MenuItem(String name) {
+    public MenuItem(String name, double basePrice) {
         this.name = name;
+        this.basePrice = basePrice;
     }
 
     public abstract double getPrice();
 
-    @Override
-    public String toString() {
-        return name + " - $" + getPrice();
+    public abstract String getDescription();
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
     }
 }
