c606308634621e0f790a89522e62694e63bd3578
arone415 <105476658+arone415@users.noreply.github.com>
2025-11-14 04:13:18 -0800

    created a nice welcome display with ASCII

--- untitled/src/main/java/com/bussinburgers/Main.java
+++ untitled/src/main/java/com/bussinburgers/Main.java
@@ -9,10 +9,10 @@ public class Main {
         ui.start();
     }
     private static void printWelcomeBanner() {
-        System.out.println("=====================================");
-        System.out.println("        WELCOME TO BUSSIN BURGERS     ");
-        System.out.println("     \"The ultimate burger experience\"  ");
-        System.out.println("=====================================");
+        System.out.println("=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿");
+        System.out.println("        \uD835\uDD4E\uD835\uDD56\uD835\uDD5D\uD835\uDD54\uD835\uDD60\uD835\uDD5E\uD835\uDD56 \uD835\uDD65\uD835\uDD60 \uD835\uDD39\uD835\uDD66\uD835\uDD64\uD835\uDD64\uD835\uDD5A\uD835\uDD5F \uD835\uDD39\uD835\uDD66\uD835\uDD63\uD835\uDD58\uD835\uDD56\uD835\uDD63\uD835\uDD64     ");
+        System.out.println("     \uD835\uDD4B\uD835\uDD59\uD835\uDD56 \uD835\uDD66\uD835\uDD5D\uD835\uDD65\uD835\uDD5A\uD835\uDD5E\uD835\uDD52\uD835\uDD65\uD835\uDD56 \uD835\uDD53\uD835\uDD66\uD835\uDD63\uD835\uDD58\uD835\uDD56\uD835\uDD63 \uD835\uDD56\uD835\uDD69\uD835\uDD61\uD835\uDD56\uD835\uDD63\uD835\uDD5A\uD835\uDD56\uD835\uDD5F\uD835\uDD54\uD835\uDD56  ");
+        System.out.println("=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿=̿");
         System.out.println();
     }
 }
