28eb5aba8e01e65bdd1b989bb43a32f6ae77b9fa
arone415 <105476658+arone415@users.noreply.github.com>
2025-11-14 02:23:53 -0800

    added simple welcome banner

--- untitled/src/main/java/com/bussinburgers/Main.java
+++ untitled/src/main/java/com/bussinburgers/Main.java
@@ -7,5 +7,12 @@ public class Main {
         UserInterface ui = new UserInterface();
         ui.start();
     }
-
+    private static void printWelcomeBanner() {
+        System.out.println("=====================================");
+        System.out.println("        WELCOME TO BUSSIN BURGERS     ");
+        System.out.println("     \"The ultimate burger experience\"  ");
+        System.out.println("=====================================");
+        System.out.println();
+    }
 }
+
